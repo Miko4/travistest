799684384147ce1a0cd22e635276aa4261939248
Miko <Miko4@users.noreply.github.com>
2016-04-20 21:15:38 +0200

    use current opam version

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,9 @@
-echo Y|sudo apt-get  install opam darcs git
+echo Y|sudo apt-get  install  darcs git
+
+# opam from debian is too old
+
+
+wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
 opam init
 eval `opam config env`
 opam switch install 3.10.2
