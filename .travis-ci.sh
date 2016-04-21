5cb69b69c2e710cf27ecad2eadddb087b0e74fcb
Your Name <you@example.com>
2016-04-21 11:42:09 +0200

    or not.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,12 +4,12 @@
 # on travis-ci. Maybe on vexor.io not?
 #sudo chmod 777 /usr/local/bin
 
-#wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
-sudo apt-get install opam
+wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
+#apt-get install opam
 
 yes Y|opam init
 eval `opam config env`
-opam switch install -y 3.10.2
+opam switch -y  install 3.10.2
 opam install -y omake
 
 ocaml test.ml
